f265e55ec2df819d26d38c87192886380ebe6ef2
qchen <q@qchendeMacBook-Pro.local>
2019-03-05 14:47:45 +0800

    first commit

---
+++ src/lib/baidu.js
@@ -0,0 +1,13 @@
+import request from 'request'
+
+class Baidu {
+
+	constructor() {
+	}
+
+	let getCordinate = (url, query, region, cityLimit, location, coordType, retCoordtype, output, ak, sn, timestamp, callback) => {
+		request(url, (error, response, body) => {
+			
+		})
+	}
+}
