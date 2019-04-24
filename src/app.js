c87e3c160d8a5888b0f3b1f288049a894de1e9fa
qchen666 <cqbus405@gmail.com>
2019-04-24 19:12:47 +0800

    允许跨域访问

--- src/app.js
+++ src/app.js
@@ -11,6 +11,14 @@ var port = 3000
 
 var app = express()
 
+//设置服务器跨域权限
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+  next();
+})
+
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({
   extended: false
