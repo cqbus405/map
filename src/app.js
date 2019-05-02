979b607644f0e1fe5396165159f50ecefa6e2856
q <cqbus405@gmail.com>
2019-05-02 11:23:22 +0800

    cors

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ var app = express()
 //设置服务器跨域权限
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild");
   res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
   next();
 })
